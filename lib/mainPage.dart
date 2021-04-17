de9ff10aca8fc78600c845faa242e6a53278c5aa
Sean <xunyun91@gmail.com>
2021-04-17 17:04:34 +0900

    CHANGE Sliding Panel

--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sliding_up_panel/flutter_sliding_up_panel.dart';
+import 'package:intl/intl.dart';
+import 'package:flutter_sliding_up_panel/sliding_up_panel_widget.dart';
+import 'package:sliding_up_panel/sliding_up_panel.dart';
 
 class Todo {
   bool isDone = false;
@@ -28,22 +31,34 @@ class _mainPageState extends State<mainPage> {
   ScrollController scrollController;
   SlidingUpPanelController panelController = SlidingUpPanelController();
 
+  final double _initFabHeight = 120.0;
+  double _fabHeight;
+  double _panelHeightOpen;
+  double _panelHeightClosed = 95.0;
+
   @override
   void initState() {
-    scrollController = ScrollController();
-    scrollController.addListener(() {
-      if (scrollController.offset >=
-              scrollController.position.maxScrollExtent &&
-          !scrollController.position.outOfRange) {
-        panelController.expand();
-      } else if (scrollController.offset <=
-              scrollController.position.minScrollExtent &&
-          !scrollController.position.outOfRange) {
-        panelController.anchor();
-      } else {}
-    });
     super.initState();
+
+    _fabHeight = _initFabHeight;
   }
+
+  // @override
+  // void initState() {
+  //   scrollController = ScrollController();
+  //   scrollController.addListener(() {
+  //     if (scrollController.offset >=
+  //             scrollController.position.maxScrollExtent &&
+  //         !scrollController.position.outOfRange) {
+  //       panelController.expand();
+  //     } else if (scrollController.offset <=
+  //             scrollController.position.minScrollExtent &&
+  //         !scrollController.position.outOfRange) {
+  //       panelController.anchor();
+  //     } else {}
+  //   });
+  //   super.initState();
+  // }
 
   final _items = <Todo>[];
 
@@ -89,6 +104,7 @@ class _mainPageState extends State<mainPage> {
   }
 
   Future<void> showDateDialog(BuildContext context) async {
+    final DateFormat _dateFormatter = DateFormat('MM, dd, yyyy');
     final newDate = await showDatePicker(
         context: context,
         initialDate: _date,
@@ -100,6 +116,7 @@ class _mainPageState extends State<mainPage> {
         _date = newDate;
       });
     }
+
     return Scaffold(
         body: Column(
       children: [
@@ -117,146 +134,132 @@ class _mainPageState extends State<mainPage> {
 
   @override
   Widget build(BuildContext context) {
-    return Stack(
-      children: <Widget>[
-        Scaffold(
-          bottomNavigationBar: BottomAppBar(
-            child: Row(
-              children: [
-                IconButton(
-                  icon: Icon(Icons.menu),
-                  onPressed: () {},
-                ),
-                Spacer(),
-                IconButton(
-                  icon: Icon(Icons.search),
-                  onPressed: () {},
-                ),
-                IconButton(
-                  icon: Icon(Icons.more_vert),
-                  onPressed: () {},
-                ),
-              ],
+    _panelHeightOpen = MediaQuery.of(context).size.height * .80;
+
+    return Scaffold(
+      bottomNavigationBar: BottomAppBar(
+        child: Row(
+          children: [
+            IconButton(
+              icon: Icon(Icons.menu),
+              onPressed: () {},
             ),
-            shape: CircularNotchedRectangle(),
-          ),
-          floatingActionButton: InkWell(
-              splashColor: Colors.blue,
-              onLongPress: () {
-                _routinePopUpScreen(context);
-              },
-              child: FloatingActionButton(
-                child: Icon(Icons.add),
-                mini: true,
-                onPressed: () {
-                  _taskPopUpScreen(context);
-                },
-              )),
-          floatingActionButtonLocation:
-              FloatingActionButtonLocation.centerDocked,
-          body: SlidingUpPanelWidget(
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 15.0),
-              decoration: ShapeDecoration(
-                color: Colors.white,
-                shadows: [
-                  BoxShadow(
-                    blurRadius: 5.0,
-                    spreadRadius: 2.0,
-                    color: Colors.white,
-                  ),
-                ],
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(10.0),
-                    topRight: Radius.circular(10.0),
-                  ),
-                ),
-              ),
-              child: Column(
-                children: <Widget>[
-                  Container(
-                    alignment: Alignment.center,
-                    child: Row(
-                      children: <Widget>[
-                        Icon(
-                          Icons.menu,
-                          size: 30,
-                        ),
-                        Padding(
-                          padding: EdgeInsets.only(left: 8.0),
-                        ),
-                        Text('Click or Drag'),
-                      ],
-                      mainAxisAlignment: MainAxisAlignment.center,
-                    ),
-                  ),
-                  Divider(
-                    height: 0.5,
-                    color: Colors.grey[300],
-                  ),
-                  Flexible(
-                    child: Container(
-                      child: ListView.separated(
-                        controller: scrollController,
-                        physics: ClampingScrollPhysics(),
-                        itemBuilder: (BuildContext context, todo) {
-                          return ListBody(
-                            children: _items
-                                .map((todo) => _buildItemWidget(todo))
-                                .toList(),
-                          );
-                        },
-                        separatorBuilder: (context, todo) {
-                          return Divider(
-                            height: 0.5,
-                          );
-                        },
-                        shrinkWrap: true,
-                        itemCount: 1,
-                      ),
-                      color: Colors.white,
-                    ),
-                  ),
-                ],
-                mainAxisSize: MainAxisSize.min,
-              ),
+            Spacer(),
+            IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {},
             ),
-            controlHeight: 50.0,
-            anchor: 0.4,
-            panelController: panelController,
-            onTap: () {
-              if (SlidingUpPanelStatus.expanded == panelController.status) {
-                panelController.collapse();
-              } else {
-                panelController.expand();
-              }
+            IconButton(
+              icon: Icon(Icons.more_vert),
+              onPressed: () {},
+            ),
+          ],
+        ),
+        shape: CircularNotchedRectangle(),
+      ),
+      floatingActionButton: InkWell(
+          splashColor: Colors.blue,
+          onLongPress: () {
+            _routinePopUpScreen(context);
+          },
+          child: FloatingActionButton(
+            child: Icon(Icons.add),
+            mini: true,
+            onPressed: () {
+              _taskPopUpScreen(context);
             },
-            enableOnTap: true,
-          ),
-        )
-      ],
+          )),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+      body: SlidingUpPanel(
+        margin: EdgeInsets.only(left: 15, right: 15),
+        maxHeight: _panelHeightOpen,
+        minHeight: _panelHeightClosed,
+        parallaxEnabled: true,
+        parallaxOffset: .5,
+        body: _body(),
+        panelBuilder: (sc) => _panel(sc),
+        borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(18.0), topRight: Radius.circular(18.0)),
+        onPanelSlide: (double pos) => setState(() {
+          _fabHeight =
+              pos * (_panelHeightOpen - _panelHeightClosed) + _initFabHeight;
+        }),
+      ),
     );
   }
 
+  Widget _panel(ScrollController sc) {
+    return MediaQuery.removePadding(
+      context: context,
+      removeTop: true,
+      child: ListView(
+        controller: sc,
+        children: <Widget>[
+          SizedBox(
+            height: 8.0,
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Container(
+                width: 50,
+                height: 5,
+                decoration: BoxDecoration(
+                  color: Colors.grey[300],
+                  borderRadius: BorderRadius.all(Radius.circular(12.0)),
+                ),
+              ),
+            ],
+          ),
+          ListView.separated(
+            physics: ClampingScrollPhysics(),
+            itemBuilder: (BuildContext context, todo) {
+              return ListBody(
+                  children:
+                      _items.map((todo) => _buildItemWidget(todo)).toList());
+            },
+            separatorBuilder: (context, todo) {
+              return Divider(
+                color: Colors.grey,
+                height: 0.1,
+              );
+            },
+            shrinkWrap: true,
+            itemCount: 1,
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget _body() {
+    return Container();
+  }
+
+
   Widget _buildItemWidget(Todo todo) {
     return ListTile(
-        onTap: () => _toggleTodo(todo),
-        title: Text(
-          todo.title,
-          style: todo.isDone
-              ? TextStyle(
-                  decoration: TextDecoration.lineThrough,
-                  fontStyle: FontStyle.italic,
-                )
-              : null,
-        ),
-        trailing: IconButton(
-          icon: Icon(Icons.delete_forever),
-          onPressed: () {
-            _deleteTodo(todo);
-          },
-        ));
+      onTap: () => _toggleTodo(todo),
+      trailing: Checkbox(
+        onChanged: (value) {
+          print(value);
+        },
+        activeColor: Theme.of(context).primaryColor,
+        value: false,
+      ),
+      title: Text(
+        todo.title,
+        style: todo.isDone
+            ? TextStyle(
+                decoration: TextDecoration.lineThrough,
+                fontStyle: FontStyle.italic,
+              )
+            : null,
+      ),
+      subtitle: Text(
+        _date.toString(),
+      ),
+    );
   }
 
   void _taskPopUpScreen(BuildContext context) {
@@ -286,16 +289,6 @@ class _mainPageState extends State<mainPage> {
             ),
             Column(
               children: [
-                TextFormField(
-                  decoration: InputDecoration(
-                    border: InputBorder.none,
-                    icon: Icon(Icons.notes),
-                    hintText: 'Description',
-                    hintStyle: TextStyle(fontSize: 15, color: Colors.grey),
-                  ),
-                  textAlignVertical: TextAlignVertical.center,
-                  style: TextStyle(fontSize: 18),
-                ),
                 Divider(),
                 Row(
                   mainAxisAlignment: MainAxisAlignment.start,
@@ -312,6 +305,7 @@ class _mainPageState extends State<mainPage> {
                         showTimeDialog();
                       },
                     ),
+                    Spacer(),
                     TextButton(
                       child: Text('Save'),
                       onPressed: () => _addTodo(Todo(_todoController.text)),
